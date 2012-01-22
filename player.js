68747fe42a24740a03776ded404e04eecfa16e2b
Finn Stokes <finn.stokes@gmail.com>
2012-01-23 00:56:33 +1030

    Added untested server

---
+++ player.js
@@ -0,0 +1,9 @@
+exports.create = function(spec,my) {
+	var that, city;
+	my = my || {};
+	
+	that = {};
+	city = spec.city || null;
+
+	return that;
+}
---
+++ server.js
@@ -0,0 +1,122 @@
+var app = require('express').createServer();
+var fs  = require('fs');
+
+var builderIo = require('socket.io').listen(49989);
+var rescueIo   = require('socket.io').listen(49990);
+
+var city   = require('./city.js');
+var player = require('./player.js');
+
+var serveFile = function (req, res, path, mimetype) {
+    fs.readFile(__dirname + path + req.params.file + '.' + req.params.ext,
+        function (err, data) {
+            if (err) {
+            res.writeHead(500);
+            return res.end('Error loading ' + req.params.file+'.'+req.params.ext);
+		}
+		
+		var mimetype = '';
+		if (req.params.ext == 'html') mimetype = 'text/html';
+		if (req.params.ext == 'css') mimetype = 'text/css';
+		if (req.params.ext == 'js') mimetype = 'text/javascript';
+		if (req.params.ext == 'png') mimetype = 'image/png';
+		res.writeHead(200, { 'Content-Type': mimetype });
+		res.end(data, 'utf-8');
+	})
+};
+
+app.get('/builder/', function (req, res){
+    console.log(req.url);
+    req.params.file = 'index';
+    req.params.ext = 'html';
+    serveFile(req, res, '/builder/');
+});
+
+app.get('/builder/:file.:ext', function (req, res){
+    console.log(req.url);
+    serveFile(req, res, '/builder/');
+});
+
+app.get('/builder/img/:file.:ext', function (req, res){
+    console.log(req.url);
+    serveFile(req, res, '/builder/img/');
+});
+
+app.get('/builder/img/tileset/:file.:ext', function (req, res){
+    console.log(req.url);
+    serveFile(req, res, '/builder/img/tileset/');
+});
+
+app.get('/rescue/', function (req, res){
+    console.log(req.url);
+    req.params.file = 'index';
+    req.params.ext = 'html';
+    serveFile(req, res, '/rescue/');
+});
+
+app.get('/rescue/:file.:ext', function (req, res){
+    console.log(req.url);
+    serveFile(req, res, '/rescue/');
+});
+
+app.get('/rescue/img/:file.:ext', function (req, res){
+    console.log(req.url);
+    serveFile(req, res, '/rescue/img/');
+});
+
+app.get('/rescue/img/tileset/:file.:ext', function (req, res){
+    console.log(req.url);
+    serveFile(req, res, '/rescue/img/tileset/');
+});
+
+app.listen(80);
+
+builderIo.sockets.on('connection', function (socket) {
+	var p = player.create({'socket': socket});
+		
+	socket.on('disconnect', function () {
+		city.free('ruined',p.city);
+		delete p;
+	});
+	
+	socket.on('setcity', function (oldCity) {
+		if (oldCity && city.add('infected', oldCity)) {
+			city.remove('ruined', p.city);
+			p.city = null;
+		} else {
+			console.log('Error setting city');
+		}
+	});
+
+	socket.on('getcity', function () {
+		if (p.city === null) {
+			p.city = city.random('ruined');
+		}
+		socket.emit('city', city.get('ruined',p.city));
+	});
+});
+
+rescueIo.sockets.on('connection', function (socket) {
+	var p = player.create({'socket': socket});
+		
+	socket.on('disconnect', function () {
+		city.free('infected',p.city);
+		delete p;
+	});
+	
+	socket.on('setcity', function (oldCity) {
+		if (oldCity && city.add('ruined', oldCity)) {
+			city.remove('infected', p.city);
+			p.city = null;
+		} else {
+			console.log('Error setting city');
+		}
+	});
+
+	socket.on('getcity', function () {
+		if (p.city === null) {
+			p.city = city.random('infected');
+		}
+		socket.emit('city', city.get('infected',p.city));
+	});
+});
